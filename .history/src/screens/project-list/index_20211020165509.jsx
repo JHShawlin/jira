283d40f8b7a8d236d2347a81ca987f8b28a80eb4
JHShawlin <981755938@qq.com>
2021-10-20 21:49:45 +0800

    begin

---
+++ .history/src/screens/project-list/index_20211020165509.jsx
@@ -0,0 +1,35 @@
+import React from "react"
+import { useState, useEffect } from "react"
+import { List } from "./list"
+import { SearchPanel } from "./search-panel"
+
+const apiUrl = process.env.REACT_APP_API_URL
+export const ProjectListScreen = () => {
+    const [param, setParam] = useState({
+        name: '',
+        personId: ''
+    })
+    const [list, setList] = useState([])
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        fetch(`${apiUrl}/projects`).then(async response => {
+            if (response.ok) {
+                setList(await response.json())            
+            }
+        })
+    }, [param])
+
+    useEffect(() => {
+        fetch(`${apiUrl}/users`).then(async response => {
+            if (response.ok) {
+                setUsers(await response.json())            
+            }
+        })
+    }, [users])
+
+    return <div>
+        <SearchPanel users={users} param={param} setParam={setParam}/>
+        <List users={users} list={list}/>
+    </div>
+}
---
+++ src/utils/index.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react"
+
+export const isFalsy = (value) => value === 0 ? false : !value
+
+export const cleanObject = (object) => {
+    const result = {...object}
+    Object.keys(result).forEach(key => {
+        const value = result[key]
+        if (isFalsy(value)) {
+            delete result[key]
+        }
+    })
+    return result
+}
+
+export const useMount = (callback) => {
+    useEffect(() => {
+        callback()
+    }, [])
+}
+
+// export const debounce = (param, delay) => {
+//     let timeout;
+//     return (...param) => {
+//         if (timeout) {
+//             clearTimeout(timeout);
+//         }
+//         timeout = setTimeout(function() {
+//             func(...param);
+//         }, delay)
+//     }
+// }
+
+export const useDebounce = (value, delay) => {
+    const [debouncedValue, setDebouncedValue] = useState(value)
+
+    useEffect(() => {
+        const timeout = setTimeout(() => setDebouncedValue(value), delay)
+        return () => clearTimeout(timeout)
+    }, [value, delay])
+
+    return debouncedValue
+}
